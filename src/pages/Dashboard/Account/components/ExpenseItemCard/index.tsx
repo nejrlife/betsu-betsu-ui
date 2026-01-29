783e139f6b4297a8ba01d86f3d7e5bdcc0a40968
Nixon Edora Jr <NixonJr.Edora@lumen.com>
2026-01-29 21:36:54 +0800

    Edit button initial

--- src/pages/Dashboard/Account/components/ExpenseItemCard/index.tsx
+++ src/pages/Dashboard/Account/components/ExpenseItemCard/index.tsx
@@ -1,5 +1,5 @@
 import "./ExpenseItemCard.less";
-// import { useState } from "react";
+import { useState } from "react";
 // import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -7,8 +7,20 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import EditButton from '@mui/icons-material/Edit';
+import DeleteButton from '@mui/icons-material/Delete';
+import { IconButton } from "@mui/material";
 
 const ExpenseItemCard = (props: any) => {
+  const [isEditing, setEditing] = useState<boolean>(false);
+
+  const handleEdit = (event: any) => {
+    event.preventDefault();
+    setEditing(true);
+    console.log("Mene");
+    console.log(props);
+  }
+  
   return (
     <Card
       variant="outlined"
@@ -29,14 +41,59 @@ const ExpenseItemCard = (props: any) => {
           boxShadow:
             "0 18px 38px rgba(0,0,0,0.14), 0 4px 10px rgba(0,0,0,0.08)",
         },
+        "& .hover-btn": {
+          opacity: 0,
+          pointerEvents: "none",
+          transition: "opacity 0.2s ease",
+        },
+        "&:hover .hover-btn": {
+          opacity: 1,
+          pointerEvents: "auto",
+        }
     }}>
       <CardContent
         sx={{
-          padding: 2,
+          pt: 0,
+          pb: 2,
+          pl: 2,
+          pr: 2,
           // pb: 0,
-          "&:last-child": { pb: 0 },
+          "&:last-child": { pb: 0 }
         }}>
         {/* <Box sx={{ flexGrow: 1 }}> */}
+        <Box
+          className="globalFlexRow"
+          sx={{
+            justifyContent: 'flex-end',
+            gap: 0
+          }}>
+          <IconButton
+            className="hover-btn"
+            sx={{
+              position: 'relative',
+              bottom: '1rem',
+              maxHeight: 40,
+              maxWidth: '3rem',
+            }}
+            onClick={handleEdit}
+            aria-label="editButton"
+          >
+            <EditButton />
+          </IconButton>
+          <IconButton
+            className="hover-btn"
+            sx={{
+              position: 'relative',
+              bottom: '1rem',
+              maxHeight: 40,
+              maxWidth: '3rem',
+            }}
+            onClick={handleEdit}
+            aria-label="deleteButton"
+          >
+            <DeleteButton />
+          </IconButton>
+        </Box>
         <Grid container spacing={2}>
           <Grid
             className="gridItem"
